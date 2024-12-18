594422b8696bcd4437d2fe8e5bda83a255301791
Temirbolat Maratuly <snake2009003@mail.ru>
2024-12-18 15:15:15 +0500

    [Final] Quick commit

--- Final/final/app/index.tsx
+++ Final/final/app/index.tsx
@@ -1,15 +1,14 @@
-import { Text, View } from "react-native";
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function Index() {
+export default function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to our Store!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex:1, justifyContent:'center', alignItems:'center'},
+  title: { fontSize: 24, fontWeight:'bold' }
+});
